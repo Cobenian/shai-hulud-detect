88e232dd1a1f99f096d409029b72497570420c02
pvdsteen <pvdsteen@gmail.com>
2025-12-01 09:50:18 +0100

    feat: enhance log file naming to include scan directory name

--- run-detect.sh
+++ run-detect.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-LOG_FILE="shai-hulud-report-$(date +%Y%m%d-%H%M%S).txt"
+SCAN_DIR="${1:-.}"
+DIR_NAME=$(basename "$SCAN_DIR")
+LOG_FILE="shai-hulud-report_${DIR_NAME}_$(date +%Y%m%d-%H%M%S).txt"
 ANSI_ESCAPE_REGEX=$'s/\x1B\[[0-9;]*[A-Za-z]//g'
 mkdir -p tmp
 
@@ -9,5 +11,5 @@ mkdir -p tmp
 # Improve dx as opposed to running: ./shai-hulud-detect.sh /path/to/project 2>&1 | tee "shai-hulud-report-$(date +%Y%m%d-%H%M%S).log"
 ./shai-hulud-detector.sh "$@" 2>&1 | tee >(sed -E "$ANSI_ESCAPE_REGEX" > "tmp/$LOG_FILE")
 
-echo ""
+echo
 echo "Log written to tmp/$LOG_FILE"
